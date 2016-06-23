936070620face3009f6ded78b1bdc29b0812739c
sijing <xisijing@gmail.com>
2016-06-23 21:24:12 +0200

    importar el test de texto y crear las clase según el test

---
+++ src/main/flyweight/texto/Componente.java
@@ -0,0 +1,15 @@
+package main.flyweight.texto;
+
+public class Componente {
+
+	public Object dibujar(boolean b) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public void add(Componente h) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
---
+++ src/main/flyweight/texto/FactoriaCaracter.java
@@ -0,0 +1,10 @@
+package main.flyweight.texto;
+
+public class FactoriaCaracter {
+
+	public static Object getFactoria() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
